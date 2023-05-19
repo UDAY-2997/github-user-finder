fccf38665056b1e619666dd1d70e37e60fc02d14
U-Dev <udaythakur.shely@gmail.com>
2023-05-19 12:23:24 -0500

    first commit

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Search = () => {
+  return (
+    <div className='container'>
+      <h1>GitHub Finder</h1>
+      <form>
+        <input type='text' placeholder='Enter username' 
+      </form>
+    </div>
+  )
+}
